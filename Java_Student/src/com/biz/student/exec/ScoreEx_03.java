54336d2d17e8905369b66e7bd7e6a4f32adb18cb
callor <callor@callor.com>
2020-07-07 10:43:19 +0900

    student

--- Java_Student/src/com/biz/student/exec/ScoreEx_03.java
+++ Java_Student/src/com/biz/student/exec/ScoreEx_03.java
@@ -12,14 +12,14 @@ import java.util.Random;
  * score.txt 파일로 저장
  */
 public class ScoreEx_03 {
-	
+
 	public static void main(String[] args) {
-		
+
 		List<String> scoreList = new ArrayList<String>();
 		PrintWriter outPut = null; // = System.out;
 		String outFile = "src/com/biz/student/exec/score.txt";
-		
-		// 지금부터 outFile 변수에 저장된 파일이름으로 
+
+		// 지금부터 outFile 변수에 저장된 파일이름으로
 		// text를 기록하기 위해 파일을 만들어라
 		try {
 			outPut = new PrintWriter(outFile);
@@ -27,35 +27,29 @@ public class ScoreEx_03 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		Random rnd = new Random();
-		
-		for(int i = 0 ; i < 30 ; i++) {
-			
+
+		for (int i = 0; i < 30; i++) {
+
 			int intKor = rnd.nextInt(50) + 51;
 			int intEng = rnd.nextInt(50) + 51;
 			int intMath = rnd.nextInt(50) + 51;
 
 			int intSum = intKor + intEng + intMath;
-			float floatAvg = (float)intSum / 3;
-			
+			float floatAvg = (float) intSum / 3;
+
 			// System.out.printf()
-			String score = String.format("%05d:%d:%d:%d:%d:%06.2f",
-					i+1,intKor,intEng,intMath,intSum,floatAvg);
-			
+			String score = String.format("%05d:%d:%d:%d:%d:%06.2f", i + 1, intKor, intEng, intMath, intSum, floatAvg);
+
 			scoreList.add(score);
-			
-			// outPut.println(score);
-		
+			outPut.println(score);
+
 		}
-		
-		Collections.shuffle(scoreList);
-		for(String s : scoreList) {
-			outPut.println(s);
-		}
+
 		outPut.close();
 		System.out.println("완료!!!");
-		
+
 	}
 
 }
